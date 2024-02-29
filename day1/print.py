42216f2dd17f2f42de2462fceeefc19ff91d2cac
Ragib Mondal <124962194+ragibmondal@users.noreply.github.com>
2024-03-01 00:22:17 +0600

    Create print.py

---
+++ day1/print.py
@@ -0,0 +1,14 @@
+print("""
+Mark
+September 20th 2029
+I am singing up for Rwplits 100 days of python 
+challenges!
+I will make sure to spend some time every day coding
+along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do
+this from my phone wherever I happen to be. No excuses
+for not coding from the middle of a field!
+I am feeling :)
+You can follow my progress at replit.com/@Hasanmuhammad7
+/day-1100-days 
+""")
